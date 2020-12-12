4e6f1398c58fff3b3f31624675cb96d35e58110f
chanduka <ashainxsamarasinghe@gmail.com>
2020-12-12 20:29:33 +0530

    edited

--- src/App.js
+++ src/App.js
@@ -39,7 +39,8 @@ const App = () => {
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
               <p>
-                Edit <code>src/App.js</code> and save to reload.
+                Edit <code>src/App.js</code> and save to reload to enter the
+                app.
               </p>
               <AmplifySignOut />
               <a
